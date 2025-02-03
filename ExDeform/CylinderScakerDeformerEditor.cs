321338ac69242a4891546fd29c529f97ac95a357
colloid <jack0904h3@yahoo.co.jp>
2025-02-04 04:33:02 +0900

    Add CylinderScaler

---
+++ ExDeform/CylinderScakerDeformerEditor.cs
@@ -0,0 +1,186 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+//using UnityEngine.UIElements;
+//using UnityEditor.UIElements;
+using Deform;
+using DeformEditor;
+
+namespace MeshModifier.NDMFDeform.ExDeform
+{
+	[CustomEditor (typeof(CylinderScalerDeformer)),CanEditMultipleObjects]
+	public class CylinderScakerDeformerEditor : DeformerEditor
+	{
+		private static class Content
+		{
+			public static readonly GUIContent Factor = DeformEditorGUIUtility.DefaultContent.Factor;
+			public static readonly GUIContent Radius = new GUIContent (text: "Radius", tooltip: "The cylinder radius.");
+			public static readonly GUIContent Scope = new GUIContent (text: "Scope", tooltip: "The cylinder deform scope.");
+			public static readonly GUIContent Top = DeformEditorGUIUtility.DefaultContent.Top;
+			public static readonly GUIContent Bottom = DeformEditorGUIUtility.DefaultContent.Bottom;
+			public static readonly GUIContent Axis = DeformEditorGUIUtility.DefaultContent.Axis;
+		}
+		
+		private class Properties
+		{
+			public SerializedProperty Factor;
+			public SerializedProperty Radius;
+			public SerializedProperty Scope;
+			public SerializedProperty Top;
+			public SerializedProperty Bottom;
+			public SerializedProperty Axis;
+			
+			public Properties (SerializedObject obj)
+			{
+				Factor = obj.FindProperty ("factor");
+				Radius = obj.FindProperty ("radius");
+				Scope = obj.FindProperty ("scope");
+				Top = obj.FindProperty ("top");
+				Bottom = obj.FindProperty ("bottom");
+				Axis = obj.FindProperty ("axis");
+			}
+		}
+		
+		private Properties properties;
+			
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			properties = new Properties (serializedObject);
+		}
+			
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+				
+			serializedObject.UpdateIfRequiredOrScript();
+			EditorGUILayout.Slider(properties.Factor, 0f, 1f,Content.Factor);
+			EditorGUILayout.PropertyField(properties.Radius,Content.Radius);
+			EditorGUILayout.PropertyField(properties.Scope, Content.Scope);
+			EditorGUILayout.PropertyField(properties.Top, Content.Top);
+			EditorGUILayout.PropertyField(properties.Bottom, Content.Bottom);
+			EditorGUILayout.PropertyField(properties.Axis,Content.Axis);
+				
+			serializedObject.ApplyModifiedProperties();
+				
+			EditorApplication.QueuePlayerLoopUpdate();
+		}
+		
+		public override void OnSceneGUI() {
+			base.OnSceneGUI();
+			
+			if (target == null) return;
+			
+			var cylinderscaler = target as CylinderScalerDeformer;
+			
+			DrawRadiusHandle(cylinderscaler);
+			DrawScopeHandle(cylinderscaler);
+			
+			
+			EditorApplication.QueuePlayerLoopUpdate();
+		}
+		
+		private void DrawRadiusHandle(CylinderScalerDeformer cylinderscaler)
+		{
+			var positon = Vector3.down * cylinderscaler.Radius;
+			
+			var axis = cylinderscaler.Axis;
+			using (new Handles.DrawingScope(Matrix4x4.TRS(axis.position, axis.rotation, axis.lossyScale)))
+			{
+				var size = HandleUtility.GetHandleSize(positon) * DeformEditorSettings.ScreenspaceSliderHandleCapSize;
+				
+				DeformHandles.Circle(Vector3.forward * cylinderscaler.Top, Vector3.forward, Vector3.up, cylinderscaler.Radius);
+				DeformHandles.Circle(Vector3.forward * cylinderscaler.Bottom, Vector3.forward, Vector3.up, cylinderscaler.Radius);
+				
+				DeformHandles.Line(positon + Vector3.forward * size, positon + Vector3.forward * cylinderscaler.Top, DeformHandles.LineMode.Light);
+				DeformHandles.Line(positon - Vector3.forward * size, positon + Vector3.forward * cylinderscaler.Bottom, DeformHandles.LineMode.Light);
+				
+				using (var check = new EditorGUI.ChangeCheckScope
+				())
+				{
+					var newWorldPosition = DeformHandles.Slider(positon, Vector3.up);
+					if (check.changed)
+					{
+						Undo.RecordObject(cylinderscaler, "Changed Radius");
+						cylinderscaler.Radius = -newWorldPosition.y;
+					}
+				}
+				
+				using (var check = new EditorGUI.ChangeCheckScope
+				())
+				{
+					var newWorldPosition = DeformHandles.Slider(Vector3.zero + Vector3.forward * cylinderscaler.Top, Vector3.forward);
+					if (check.changed)
+					{
+						Undo.RecordObject(cylinderscaler, "Changed Top");
+						cylinderscaler.Top = newWorldPosition.z;
+					}
+				}
+				
+				using (var check = new EditorGUI.ChangeCheckScope
+				())
+				{
+					var newWorldPosition = DeformHandles.Slider(Vector3.zero + Vector3.forward * cylinderscaler.Bottom, Vector3.forward);
+					if (check.changed)
+					{
+						Undo.RecordObject(cylinderscaler, "Changed Bottom");
+						cylinderscaler.Bottom = newWorldPosition.z;
+					}
+				}
+			}
+		}
+		
+		private void DrawScopeHandle(CylinderScalerDeformer cylinderscaler)
+		{
+			var positon = Vector3.down * cylinderscaler.Scope;
+			
+			var axis = cylinderscaler.Axis;
+			using (new Handles.DrawingScope(Matrix4x4.TRS(axis.position, axis.rotation, axis.lossyScale)))
+			{
+				var size = HandleUtility.GetHandleSize(positon) * DeformEditorSettings.ScreenspaceSliderHandleCapSize;
+				
+				DeformHandles.Circle(Vector3.forward * cylinderscaler.Top, Vector3.forward, Vector3.up, cylinderscaler.Scope);
+				DeformHandles.Circle(Vector3.forward * cylinderscaler.Bottom, Vector3.forward, Vector3.up, cylinderscaler.Scope);
+				
+				DeformHandles.Line(positon + Vector3.forward * size, positon + Vector3.forward * cylinderscaler.Top, DeformHandles.LineMode.LightDotted);
+				DeformHandles.Line(positon - Vector3.forward * size, positon + Vector3.forward * cylinderscaler.Bottom, DeformHandles.LineMode.LightDotted);
+				
+				using (var check = new EditorGUI.ChangeCheckScope
+				())
+				{
+					var newWorldPosition = DeformHandles.Slider(positon, Vector3.up);
+					if (check.changed)
+					{
+						Undo.RecordObject(cylinderscaler, "Changed Scope");
+						cylinderscaler.Scope = -newWorldPosition.y;
+					}
+				}
+				
+				//using (var check = new EditorGUI.ChangeCheckScope
+				//())
+				//{
+				//	var newWorldPosition = DeformHandles.Slider(positon + Vector3.forward * cylinderscaler.Top, Vector3.forward);
+				//	if (check.changed)
+				//	{
+				//		Undo.RecordObject(cylinderscaler, "Changed Top");
+				//		cylinderscaler.Top = newWorldPosition.z;
+				//	}
+				//}
+				
+				//using (var check = new EditorGUI.ChangeCheckScope
+				//())
+				//{
+				//	var newWorldPosition = DeformHandles.Slider(positon + Vector3.forward * cylinderscaler.Bottom, Vector3.forward);
+				//	if (check.changed)
+				//	{
+				//		Undo.RecordObject(cylinderscaler, "Changed Bottom");
+				//		cylinderscaler.Bottom = newWorldPosition.z;
+				//	}
+				//}
+			}
+		}
+	}
+}
---
+++ ExDeform/CylinderScalerDeformer.cs
@@ -0,0 +1,111 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Mathematics;
+using System.Linq;
+using UnityEngine;
+//using UnityEditor;
+using Deform;
+using static Unity.Mathematics.math;
+using float3 = Unity.Mathematics.float3;
+using float4x4 = Unity.Mathematics.float4x4;
+
+namespace MeshModifier.NDMFDeform.ExDeform
+{
+	[Deformer(Name = "Cylinder Scaler", Description = "Scale deform a mesh using cylinder controler", XRotation = 90f, Type = typeof(CylinderScalerDeformer))]
+	public class CylinderScalerDeformer : Deformer, IFactor
+	{
+		public float Factor
+		{
+			get => factor;
+			set => factor = Mathf.Clamp01 (value);
+		}
+		public float Radius
+		{
+			get => radius;
+			set => radius = value;
+		}
+		public float Scope
+		{
+			get => scope;
+			set => scope = value;
+		}
+		public float Top
+		{
+			get => top;
+			set => top = value;
+		}
+		public float Bottom
+		{
+			get => bottom;
+			set => bottom = value;
+		}
+		public Transform Axis
+		{
+			get
+			{
+				if (axis == null)
+					axis = transform;
+				return axis;
+			}
+			set => axis = value;
+		}
+		
+		[SerializeField, HideInInspector] private float factor = 0f;
+		[SerializeField, HideInInspector] private float radius = 1f;
+		[SerializeField, HideInInspector] private float scope = 1f;
+		[SerializeField, HideInInspector] private float top = 0.5f;
+		[SerializeField, HideInInspector] private float bottom = -0.5f;
+		[SerializeField, HideInInspector] private Transform axis;
+		
+		public override DataFlags DataFlags => Deform.DataFlags.Vertices;
+		
+		public override JobHandle Process(MeshData data, JobHandle dependency = default) {
+			if (Mathf.Approximately(Factor, 0f))
+				return dependency;
+				
+			var meshToAxis = DeformerUtils.GetMeshToAxisSpace(Axis, data.Target.GetTransform());
+			
+			return new CylinderScalerJob
+			{
+				factor = Factor,
+				radius = Radius,
+				scope = Scope,
+				top = Top,
+				bottom = Bottom,
+				meshToAxis = meshToAxis,
+				axisToMesh = meshToAxis.inverse,
+				vertices = data.DynamicNative.VertexBuffer
+			}.Schedule(data.Length, DEFAULT_BATCH_COUNT, dependency);
+		}
+		
+		public struct CylinderScalerJob : IJobParallelFor
+		{
+			public float factor;
+			public float radius;
+			public float scope;
+			public float top;
+			public float bottom;
+			public float4x4 meshToAxis;
+			public float4x4 axisToMesh;
+			public NativeArray<float3> vertices;
+			
+			public void Execute(int index)
+			{
+				var range = scope;
+				var point = mul (meshToAxis, float4 (vertices[index],1f));
+				var d = length(point.xy);
+				
+				if (d < range && point.z < top && point.z > bottom)
+				{
+					point.xy *= lerp(1f, radius/scope, factor);
+				}
+				
+				vertices[index] = mul(axisToMesh, point).xyz;
+			}
+		}
+	}
+}
